1cda149292faf6121c74fb522ed1e2fb0d3fd258
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2011-07-15 16:13:49 +0200

    Löschen repariert Begonnen, hArtEdit umzubauen, damit jede Artgruppe eigene Felder hat

--- vendor/couchapp/_attachments/jquery.mobile.actionsheet.js
+++ vendor/couchapp/_attachments/jquery.mobile.actionsheet.js
@@ -7,41 +7,44 @@
  * Date: 2011-05-03 17:11:00 (Tue, 3 May 2011)
  * Revision: 1.1
  */
-(function($,window,undefined){
+(function($,window){
 	$.widget("mobile.actionsheet",$.mobile.widget,{
 		wallpaper: undefined,
 		content: undefined,
 		_init: function() {
 			var self = this;
-			this.content= this.element.next('div').addClass('ui-actionsheet-content');
-			if( this.content.parents( ':jqmData(role="content")' ).length == 0 ) {
+			this.content = ((typeof this.element.jqmData('sheet') !== 'undefined')
+				? $('#' + this.element.jqmData('sheet'))
+				: this.element.next('div'))
+				.addClass('ui-actionsheet-content');
+			if( this.content.parents( ':jqmData(role="content")' ).length === 0 ) {
 				// sheet-content is not part of the page-content,
 				// maybe it's part of the page-header: move it to page-content!
-				var currentPage = this.content.parents(':jqmData(role="page")');
-				var currentContent = currentPage.children(':jqmData(role="content")');
+				var currentContent = 
+					this.content.parents(':jqmData(role="page")').children(':jqmData(role="content")');
 				this.content.remove().appendTo(currentContent);
 			}
 			//setup command buttons
 			this.content.find(':jqmData(role="button")').filter(':jqmData(rel!="close")')
 				.addClass('ui-actionsheet-commandbtn')
-				.bind('click', function(event){
+				.bind('tap', function(){
 					self.reset();
 				});
 			//setup close button
 			this.content.find(':jqmData(rel="close")')
 				.addClass('ui-actionsheet-closebtn')
-				.bind('click', function(event){
+				.bind('tap', function(){
 					self.close();
 				});
-			this.element.bind('click', function(){
+			this.element.bind('tap', function(){
 				self.open();
 			});
-			if( this.element.parents( ':jqmData(role="content")' ).length != 0 ) {
+			if( this.element.parents( ':jqmData(role="content")' ).length !== 0 ) {
 				this.element.buttonMarkup();
 			}
 		},
 		open: function() {
-			this.element.unbind('click'); //avoid twice opening
+			this.element.unbind('tap'); //avoid twice opening
 			
 			var cc= this.content.parents(':jqmData(role="content")');
 			this.wallpaper= $('<div>', {'class':'ui-actionsheet-wallpaper'})
@@ -49,7 +52,7 @@
 				.show();
  
 			window.setTimeout(function(self) {
-				self.wallpaper.bind('click', function(event) {
+				self.wallpaper.bind('tap', function() {
 					self.close();
 				});
 			}, 500, this);
@@ -66,19 +69,19 @@
 			this.content.addClass("ui-actionsheet-animateIn").show();
 		},
 		close: function() {
-			var self= this;
-			this.wallpaper.unbind('click');
+			var self = this;
+			this.wallpaper.unbind('tap');
 			$(window).unbind('orientationchange.actionsheet');
 			if( $.support.cssTransitions ) {
 				this.content.addClass("ui-actionsheet-animateOut");
 				this.wallpaper.remove();
-				this.content.animationComplete(function(event) {
+				this.content.animationComplete(function() {
 					self.reset();
 				});
 			} else {
 				this.wallpaper.remove();
 				this.content.fadeOut();
-				this.element.bind('click', function(){
+				this.element.bind('tap', function(){
 					self.open();
 				});
 			}
@@ -90,14 +93,14 @@
 				.removeClass("ui-actionsheet-animateIn")
 				.hide();
 			var self= this;
-			this.element.bind('click', function(){
+			this.element.bind('tap', function(){
 				self.open();
 			});
 		},
 		_positionContent: function() {
-			var height = $(window).height();
-			var width = $(window).width();
-			var scrollPosition = $(window).scrollTop();
+			var height = $(window).height(),
+				width = $(window).width(),
+				scrollPosition = $(window).scrollTop();
 			this.content.css({
 				'top': (scrollPosition + height / 2 - this.content.height() / 2),
 				'left': (width / 2 - this.content.width() / 2)
