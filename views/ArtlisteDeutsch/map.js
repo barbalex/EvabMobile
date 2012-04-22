d07814ce94acca1fd402b41e38356750501d9489
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2012-04-22 22:22:46 +0200

    Abfrage ArtlisteDeutsch: verbessert

--- views/ArtlisteDeutsch/map.js
+++ views/ArtlisteDeutsch/map.js
@@ -1,6 +1,6 @@
-﻿function(doc) {
-	var L = doc.ArtNameD.substring(0, 1);
-  	if(doc.Typ && doc.ArtBezeichnungD && doc.ArtGruppe &&(doc.Typ == 'Art' || doc.Typ == 'Unbekannte Art' || doc.Typ == 'Eigene Art')){
+﻿function(doc) {	
+  	if(doc.Typ && doc.ArtNameD && (doc.ArtNameD !== null) && doc.ArtBezeichnungD && (doc.ArtBezeichnungD !== null) && doc.ArtGruppe &&(doc.Typ == 'Art' || doc.Typ == 'Unbekannte Art' || doc.Typ == 'Eigene Art')){
+  		var L = doc.ArtNameD.substring(0, 1);
 		emit ([doc.ArtGruppe, L, doc.ArtBezeichnungD], doc);
 	}
 }
