24c1f602ae178ab00a54f8a7831984a6580e6d04
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2013-05-26 22:07:14 +0200

    Struktur angepasst, Artdaten von artendb importiert

--- views/Artliste/map.js
+++ views/Artliste/map.js
@@ -1,5 +1,5 @@
 ﻿function(doc) {
-	if(doc.Typ && doc.ArtGruppe && doc.ArtBezeichnungL &&(doc.Typ === 'Art' || doc.Typ === 'Unbekannte Art' || doc.Typ === 'Eigene Art')){
-		emit ([doc.ArtGruppe, doc.ArtBezeichnungL], doc);
+	if(doc.Typ && doc.ArtGruppe && doc.Artname &&(doc.Typ === 'Arteigenschaft' || doc.Typ === 'Unbekannte Art' || doc.Typ === 'Eigene Art')){
+		emit ([doc.ArtGruppe, doc.Artname], doc);
 	}
 }
