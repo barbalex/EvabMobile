33178dbc22908b4b4bad928e382e8337c300c5f4
Alexander Gabriel <alex.barbalex@gmail.com>
2013-12-27 20:57:35 +0100

    Export: id wird mitgeliefert

--- lists/FeldExport.js
+++ lists/FeldExport.js
@@ -7,9 +7,9 @@ function(head, req) {
 			"Accept-Charset": "utf-8"
 		}
 	});
-	send('Hierarchiestufe\tFeldName\tReihenfolge\tFeldBeschriftung\tFeldBeschreibung\tFormularelement\tInputTyp\tFeldNameEvab\tFeldNameZdsf\tFeldNameCscf\tFeldNameNism\tFeldNameWslFlechten\tFeldNameWslPilze\tArtGruppe\tOptionen\tStandardwert\tSliderMinimum\tSliderMaximum\n');
+	send('ID\tHierarchiestufe\tFeldName\tReihenfolge\tFeldBeschriftung\tFeldBeschreibung\tFormularelement\tInputTyp\tFeldNameEvab\tFeldNameZdsf\tFeldNameCscf\tFeldNameNism\tFeldNameWslFlechten\tFeldNameWslPilze\tArtGruppe\tOptionen\tStandardwert\tSliderMinimum\tSliderMaximum\n');
 	while(row = getRow()) {
 		doc = row.doc;
-		send('"' + (doc.Hierarchiestufe || "") + '"\t"' + (doc.FeldName || "") + '"\t"' + (doc.Reihenfolge || "") + '"\t"' + (doc.FeldBeschriftung || "") + '"\t"' + (doc.FeldBeschreibung || "") + '"\t"' + (doc.Formularelement || "") + '"\t"' + (doc.InputTyp || "") + '"\t"' + (doc.FeldNameEvab || "") + '"\t"' + (doc.FeldNameZdsf || "") + '"\t"' + (doc.FeldNameCscf || "") + '"\t"' + (doc.FeldNameNism || "") + '"\t"' + (doc.FeldNameWslFlechten || "") + '"\t"' + (doc.FeldNameWslPilze || "") + '"\t"' + (doc.ArtGruppe || "") + '"\t"' + (doc.Optionen || "") + '"\t"' + (doc.Standardwert || "") + '"\t"' + (doc.SliderMinimum || "") + '"\t"' + (doc.SliderMaximum || "") + '" \n');
+		send('"' + doc._id + '"\t"' + (doc.Hierarchiestufe || "") + '"\t"' + (doc.FeldName || "") + '"\t"' + (doc.Reihenfolge || "") + '"\t"' + (doc.FeldBeschriftung || "") + '"\t"' + (doc.FeldBeschreibung || "") + '"\t"' + (doc.Formularelement || "") + '"\t"' + (doc.InputTyp || "") + '"\t"' + (doc.FeldNameEvab || "") + '"\t"' + (doc.FeldNameZdsf || "") + '"\t"' + (doc.FeldNameCscf || "") + '"\t"' + (doc.FeldNameNism || "") + '"\t"' + (doc.FeldNameWslFlechten || "") + '"\t"' + (doc.FeldNameWslPilze || "") + '"\t"' + (doc.ArtGruppe || "") + '"\t"' + (doc.Optionen || "") + '"\t"' + (doc.Standardwert || "") + '"\t"' + (doc.SliderMinimum || "") + '"\t"' + (doc.SliderMaximum || "") + '" \n');
 	}
 }
